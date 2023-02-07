84d8f1144101ed0df5a900c913915f8bafc391c7
Itzik Baruch <itzik.baruch@gmail.com>
2023-02-07 16:05:07 +0200

    Changed folder location

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int targetScore;
+
+    private int _score = 0;
+
+    public bool Increase()
+    {
+        return ++_score >= targetScore;
+    }
+
+    public void Reset()
+    {
+        _score = 0;
+    }
+}
