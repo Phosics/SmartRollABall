1d438cb47a574b80a03fd2167895596c07e1eb16
Itzik Baruch <itzik.baruch@gmail.com>
2023-02-11 12:09:30 +0200

    Stage 01 - Apply pickup changes

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,23 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PickUp : MonoBehaviour
 {
-    public float speed = 1;
-    public bool isMoving = false;
-    private float defaultHeight = 0.5f;
+    [Header("Y Axis Movement")]
+    public bool isMoving;
+    public float moveSpeed;
+    
+    private const float DefaultHeight = 0.5f;
 
     public void SetStartLocation(Vector3 newLocation)
     {
-        newLocation.y = isMoving ? Random.Range(1.5f, 2.5f) : defaultHeight;
+        newLocation.y = isMoving ? Random.Range(1.5f, 2.5f) : DefaultHeight;
         transform.position = newLocation;
     }
 
-    void Update()
+    private void Update()
     {
+        var pickUpTransform = transform;
+        var pickUpPosition = pickUpTransform.position;
+        
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
-        var y = isMoving ? Mathf.PingPong((transform.position.y + Time.time) * speed, 1) * 2 - 1: transform.position.y;
-        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        pickUpTransform.position = new Vector3(pickUpPosition.x, CalculateHeight(pickUpPosition), pickUpPosition.z);
+    }
+
+    private float CalculateHeight(Vector3 position)
+    {
+        return isMoving ? Mathf.PingPong((position.y + Time.time) * moveSpeed, 1) * 2 - 1 : position.y;
     }
 }
