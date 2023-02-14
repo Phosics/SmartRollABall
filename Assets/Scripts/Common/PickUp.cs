3bd8b175b765e7b945798e93f238b2d986401893
daniel lifshitz <daniel.lifshitz@gmail.com>
2023-02-14 18:26:46 +0200

    make the coin move

--- Assets/Scripts/Common/PickUp.cs
+++ Assets/Scripts/Common/PickUp.cs
@@ -8,27 +8,28 @@ namespace Common
         [Header("Y Axis Movement")]
         public bool isMoving;
         public float moveSpeed;
+        private float Height = 0;
 
         protected const float DefaultHeight = 0.5f;
 
         public void SetLocation(Vector3 newLocation)
         {
             newLocation.y = isMoving ? Random.Range(1.5f, 2.5f) : DefaultHeight;
+            Height = newLocation.y;
             transform.position = newLocation;
         }
 
         private void Update()
         {
-            var pickUpTransform = transform;
-            var pickUpPosition = pickUpTransform.position;
+            var pickUpPosition = transform.position;
         
             transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
-            pickUpTransform.position = new Vector3(pickUpPosition.x, CalculateHeight(pickUpPosition), pickUpPosition.z);
+            transform.position = new Vector3(pickUpPosition.x, CalculateHeight(pickUpPosition), pickUpPosition.z);
         }
 
         private float CalculateHeight(Vector3 position)
         {
-            return isMoving ? Mathf.PingPong((position.y + Time.time) * moveSpeed, 1) * 2 - 1 : position.y;
+            return isMoving ? Mathf.Lerp(Height - 1, Height + 1, Mathf.PingPong(Time.time / moveSpeed, 1)) : position.y;
         }
     }
 }
