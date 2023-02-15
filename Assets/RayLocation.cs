77ff5ef648f3ec15cbaa16848b18256d1b3aa48f
daniel lifshitz <daniel.lifshitz@gmail.com>
2023-02-15 15:30:19 +0200

    disable rotating WIP

--- Assets/RayLocation.cs
+++ Assets/RayLocation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RaySensor : MonoBehaviour
+public class RayLocation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
