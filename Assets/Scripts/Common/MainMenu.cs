ef699939ef2aa9b9654f2778aaad271d7e1ba500
Itzik Baruch <itzik.baruch@gmail.com>
2023-02-25 22:36:59 +0200

    Menu Fixes

--- Assets/Scripts/Common/MainMenu.cs
+++ Assets/Scripts/Common/MainMenu.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,6 +7,18 @@ namespace Common
 {
     public class MainMenu : MonoBehaviour
     {
+        [Header("Stages")] 
+        public GameObject stage1;
+        public GameObject stage2;
+        public GameObject stage3;
+        
+        [Space(5)]
+        [Header("High Score")]
+        public GameObject highScoreMenu;
+        public HighScoreController highScoreController;
+        
+        [Space(5)]
+        [Header("Options")]
         public Toggle toggleAI;
 
         public void Start()
@@ -15,17 +28,63 @@ namespace Common
 
         public void StartStage1()
         {
+            SetTimeScale();
             SceneManager.LoadScene("Stage - 01");
         }
     
         public void StartStage2()
         {
+            SetTimeScale();
             SceneManager.LoadScene("Stage - 02");
         }
     
         public void StartStage3()
         {
+            SetTimeScale();
             SceneManager.LoadScene("Stage - 03");
+        }
+
+        public void ShowScoreboard(int stageNumber)
+        {
+            stage1.SetActive(false);
+            stage2.SetActive(false);
+            stage3.SetActive(false);
+            toggleAI.gameObject.SetActive(false);
+
+            switch (stageNumber)
+            {
+                case 1:
+                    highScoreController.scoreManager.stage = StageNumber.One;
+                    break;
+                case 2:
+                    highScoreController.scoreManager.stage = StageNumber.Two;
+                    break;
+                case 3:
+                    highScoreController.scoreManager.stage = StageNumber.Three;
+                    break;
+            }
+            
+            highScoreController.ViewScoreBoard();
+            
+            highScoreMenu.SetActive(true);
+        }
+
+        public void SuppressScoreboard()
+        {
+            stage1.SetActive(true);
+            stage2.SetActive(true);
+            stage3.SetActive(true);
+            toggleAI.gameObject.SetActive(true);
+            
+            highScoreMenu.SetActive(false);
+        }
+
+        private static void SetTimeScale()
+        {
+            if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+            }
         }
     }
 }
